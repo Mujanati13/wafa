c86f6261e05578a6b86009a37e5200ca811a0ba7
Mohammed <simojanati.1j@gmail.com>
2026-01-17 00:00:14 +0100

    C

--- wafa-frentend/src/lib/utils.js
+++ wafa-frentend/src/lib/utils.js
@@ -7,7 +7,7 @@ export function cn(...inputs) {
 }
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_BASED_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
   maxContentLength: Infinity,
